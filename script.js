eec50ce340936882173a5e71820668be5b5d5df2
saleheenshafiq9 <48786700+saleheenshafiq9@users.noreply.github.com>
2022-01-26 01:53:29 +0600

    new portfolio v1

--- script.js
+++ script.js
@@ -6,56 +6,12 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
-
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    })
-
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
     });
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-  
+    
     var typed = new Typed(".typing", {
-        strings: ["Designer", "Developer", "Data Analyst", "Freelancer"],
+        strings: ["Web Development", "UX Design", "Data Analysis", "Freelancing"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
-    var typed = new Typed(".typing-2", {
-        strings: ["Designer", "Developer", "Data Analyst", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-});
-
-$('.menu-btn').click(function(){
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i').toggleClass("active");
-}); 
-
-$('.carousel').owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-        0:{
-            items: 1,
-            nav: false
-        }
-    }
 });
